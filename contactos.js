6643f6462bbd6f65b5d645b48bc7cb0dd8f64b5e
Menfi Garcia <menfigarcia31@gmail.com>
2023-08-18 10:53:11 -0600

    contactos

---
+++ contactos.js
@@ -0,0 +1,23 @@
+let contactos = ["Mario Fernandez", "Luis Mondragon", "Maria Calderon", "Pedro Zapata", "Luisa Espinal", "Emily Bermudez"];
+
+
+function agregar(contacto) {
+    contactos.push(contacto);
+}
+
+function borrarContacto(borrar) {
+    contactos = contactos.filter(nombre => nombre !== borrar);
+}
+
+function imprimir() {
+    contactos.forEach(function (contacto) {
+        console.log(contacto);
+    });
+}
+
+
+agregar("Marisol Villanueva");
+borrarContacto("Mario Fernandez");
+imprimir();
+
+
